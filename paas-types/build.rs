8af85698de5468441f53c33bb4c5355c701d5aa7
Justinas Stankevicius <justinas@justinas.org>
2021-03-11 20:31:17 +0200

    Use Bytes

--- paas-types/build.rs
+++ paas-types/build.rs
@@ -1,4 +1,8 @@
+use prost_build::Config;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("./proto/paas.proto")?;
+    let mut config = Config::new();
+    config.bytes(&["LogsResponse.lines"]);
+    tonic_build::configure().compile_with_config(config, &["./proto/paas.proto"], &["./proto"])?;
     Ok(())
 }
