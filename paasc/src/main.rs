4f03a7bce25ac120828cfa24a844566a438bf8ee
Justinas Stankevicius <justinas@justinas.org>
2021-03-18 21:39:43 +0200

    Functional client MVP

--- paasc/src/main.rs
+++ paasc/src/main.rs
@@ -1,3 +1,4 @@
+use futures::{pin_mut, stream::StreamExt};
 use paasc::make_client;
 
 #[tokio::main]
@@ -8,13 +9,47 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let resp = client
         .exec(paas_types::ExecRequest {
-            args: vec!["echo".into(), "foo".into()],
+            args: vec![
+                "bash".into(),
+                "-c".into(),
+                "while true; do echo $RANDOM; sleep 1; done".into(),
+            ],
+            //args: vec!["echo".into(), "foo".into()],
         })
         .await?;
     dbg!(&resp);
     let id = resp.into_inner().id;
 
-    let resp = client.get_status(paas_types::StatusRequest { id }).await?;
+    tokio::time::sleep(std::time::Duration::from_secs(10)).await;
+
+    let stream = client
+        .get_logs(paas_types::LogsRequest { id: id.clone() })
+        .await?
+        .into_inner();
+
+    pin_mut!(stream);
+
+    stream
+        .for_each(|m| async move {
+            println!("{:?}", m);
+        })
+        .await;
+
+    /*
+    let resp = client
+        .get_status(paas_types::StatusRequest { id: id.clone() })
+        .await?;
     dbg!(&resp);
+
+    let resp = client
+        .stop(paas_types::StopRequest { id: id.clone() })
+        .await?;
+    dbg!(&resp);
+
+    let resp = client
+        .get_status(paas_types::StatusRequest { id: id.clone() })
+        .await?;
+    dbg!(&resp);
+    */
     Ok(())
 }
