cb7be18efbb043a5fb6d1acaf67b701be133a9a0
Justinas Stankevicius <justinas@justinas.org>
2021-03-18 17:44:23 +0200

    Bring in Process, add Process-less store for tests

--- paasd/src/store.rs
+++ paasd/src/store.rs
@@ -5,6 +5,8 @@ use std::{
 
 use uuid::Uuid;
 
+use worker::Process;
+
 use crate::user::UserId;
 
 #[derive(Debug, thiserror::Error)]
@@ -15,18 +17,19 @@ pub enum GetError {
     Unauthorized,
 }
 
-type Process = (); // TODO: worker::Process instead
-type ProcessMap = HashMap<Uuid, (UserId, Arc<Process>)>;
+pub type ProcessStore = Store<Process>;
+
+type StoreMap<V> = HashMap<Uuid, (UserId, Arc<V>)>;
 
 #[derive(Default)]
-pub struct ProcessStore(RwLock<ProcessMap>);
+pub struct Store<V>(RwLock<StoreMap<V>>);
 
-impl ProcessStore {
+impl<V> Store<V> {
     pub fn new() -> Self {
-        Default::default()
+        Self(RwLock::new(StoreMap::new()))
     }
 
-    pub fn get(&self, pid: Uuid, uid: &UserId) -> Result<Arc<Process>, GetError> {
+    pub fn get(&self, pid: Uuid, uid: &UserId) -> Result<Arc<V>, GetError> {
         let read = self.0.read().unwrap();
         let (owner, process) = read.get(&pid).ok_or(GetError::NotFound)?;
         // TODO: does this need constant-time EQ?
@@ -37,7 +40,7 @@ impl ProcessStore {
         }
     }
 
-    pub fn insert(&self, uid: &UserId, process: Process) -> Uuid {
+    pub fn insert(&self, uid: &UserId, process: V) -> Uuid {
         let pid = Uuid::new_v4();
         let prev = self
             .0
@@ -54,28 +57,29 @@ impl ProcessStore {
 #[cfg(test)]
 mod test {
     use super::*;
+    type Store = super::Store<()>;
 
     #[test]
-    fn test_pstore_get_not_found() {
+    fn test_store_get_not_found() {
         let uid = UserId("alice".into());
-        let store = ProcessStore::new();
+        let store = Store::new();
         let pid = Uuid::new_v4();
         assert!(matches!(store.get(pid, &uid), Err(GetError::NotFound)));
     }
 
     #[test]
-    fn test_pstore_get_unauthorized() {
+    fn test_store_get_unauthorized() {
         let uid1 = UserId("alice".into());
         let uid2 = UserId("eve".into());
-        let store = ProcessStore::new();
+        let store = Store::new();
         let pid = store.insert(&uid1, ());
         assert!(matches!(store.get(pid, &uid2), Err(GetError::Unauthorized)))
     }
 
     #[test]
-    fn test_pstore_get_authorized() {
+    fn test_store_get_authorized() {
         let uid = UserId("alice".into());
-        let store = ProcessStore::new();
+        let store = Store::new();
         let pid = store.insert(&uid, ());
         assert!(store.get(pid, &uid).is_ok())
     }
