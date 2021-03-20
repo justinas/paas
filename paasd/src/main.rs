55581098eea588a304f309cb73204b1cc2ee13ba
Justinas Stankevicius <justinas@justinas.org>
2021-03-20 14:19:46 +0200

    Use anyhow::Error in main()

--- paasd/src/main.rs
+++ paasd/src/main.rs
@@ -1,9 +1,9 @@
-use std::error::Error;
+use anyhow::Error;
 
 use paasd::make_server;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<(), Error> {
     pretty_env_logger::init();
     make_server()?.serve("127.0.0.1:8443".parse()?).await?;
     Ok(())
