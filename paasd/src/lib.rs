ee201de9c99b2bb8e7c07909dce4c1a2327c755b
Justinas Stankevicius <justinas@justinas.org>
2021-03-20 14:09:58 +0200

    Use anyhow instead of panics in setup code

--- paasd/src/lib.rs
+++ paasd/src/lib.rs
@@ -1,5 +1,6 @@
-use std::{error::Error, fs::File, io::BufReader, sync::Arc};
+use std::{fs::File, io::BufReader, sync::Arc};
 
+use anyhow::{anyhow, Error};
 use rustls::{
     ciphersuite, internal::pemfile, AllowAnyAuthenticatedClient, RootCertStore, ServerConfig,
     SupportedCipherSuite,
@@ -25,26 +26,28 @@ static CIPHERSUITES: &[&SupportedCipherSuite; 5] = &[
     &ciphersuite::TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 ];
 
-fn buf_read(path: &str) -> Result<BufReader<File>, Box<dyn Error>> {
+fn buf_read(path: &str) -> Result<BufReader<File>, Error> {
     Ok(BufReader::new(File::open(path)?))
 }
 
-fn rustls_config() -> Result<ServerConfig, Box<dyn Error>> {
+fn rustls_config() -> Result<ServerConfig, Error> {
     let mut cert_store = RootCertStore::empty();
     cert_store
         .add_pem_file(&mut buf_read("data/client_ca.pem")?)
-        .unwrap();
+        .map_err(|_| anyhow!("could not add client CA to store"))?;
 
-    let cert = pemfile::certs(&mut buf_read("data/server.pem")?).unwrap();
-    let key = pemfile::rsa_private_keys(&mut buf_read("data/server.key")?)
-        .unwrap()
+    let cert = pemfile::certs(&mut buf_read("data/server.pem")?)
+        .map_err(|_| anyhow!("could not parse the server certificate"))?;
+    let keys = pemfile::rsa_private_keys(&mut buf_read("data/server.key")?)
+        .map_err(|_| anyhow!("could not parse server private keys"))?;
+    let key = keys
         .into_iter()
         .next()
-        .unwrap();
+        .ok_or_else(|| anyhow!("expected at least one private key"))?;
 
     let mut config = ServerConfig::new(AllowAnyAuthenticatedClient::new(cert_store));
     config.ciphersuites = CIPHERSUITES.to_vec();
-    config.set_single_cert(cert, key).unwrap();
+    config.set_single_cert(cert, key)?;
     config.set_protocols(&[b"h2"[..].into()]);
     Ok(config)
 }
@@ -53,7 +56,7 @@ fn make_service() -> ProcessServiceServer<ProcessService> {
     ProcessServiceServer::new(ProcessService::new(Arc::new(ProcessStore::new())))
 }
 
-pub fn make_server() -> Result<Server, Box<dyn Error>> {
+pub fn make_server() -> Result<Server, Error> {
     let mut tls = ServerTlsConfig::new();
     tls.rustls_server_config(rustls_config()?);
 
