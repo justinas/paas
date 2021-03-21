2e4b50180d5f8b80a8cce71acfbdd26d2ae3c274
Justinas Stankevicius <justinas@justinas.org>
2021-03-21 17:50:35 +0200

    Use anyhow and ? instead of unwraps

--- paasc/src/lib.rs
+++ paasc/src/lib.rs
@@ -1,3 +1,4 @@
+use anyhow::{anyhow, Error};
 use rustls::{ciphersuite, internal::pemfile, ClientConfig, RootCertStore, SupportedCipherSuite};
 use std::{fs::File, io::BufReader, path::Path};
 use tonic::transport::Channel;
@@ -12,25 +13,27 @@ static CIPHERSUITES: &[&SupportedCipherSuite; 5] = &[
     &ciphersuite::TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 ];
 
-fn buf_read(path: impl AsRef<Path>) -> Result<BufReader<File>, Box<dyn std::error::Error>> {
+fn buf_read(path: impl AsRef<Path>) -> Result<BufReader<File>, Error> {
     Ok(BufReader::new(File::open(path)?))
 }
 
-fn rustls_config(client: &str) -> Result<ClientConfig, Box<dyn std::error::Error>> {
+fn rustls_config(client: &str) -> Result<ClientConfig, Error> {
     let data_path = Path::new("./data");
     let mut cert_store = RootCertStore::empty();
     cert_store
         .add_pem_file(&mut buf_read(data_path.join("server_ca.pem"))?)
-        .unwrap();
+        .map_err(|_| anyhow!("could not add server CA to store"))?;
 
     let cert_path = data_path.join(&format!("{}.pem", client));
     let key_path = data_path.join(&format!("{}.key", client));
-    let cert = pemfile::certs(&mut buf_read(&cert_path)?).unwrap();
-    let key = pemfile::rsa_private_keys(&mut buf_read(&key_path)?)
-        .unwrap()
+    let cert = pemfile::certs(&mut buf_read(&cert_path)?)
+        .map_err(|_| anyhow!("could not parse the client certificate"))?;
+    let keys = pemfile::rsa_private_keys(&mut buf_read(&key_path)?)
+        .map_err(|_| anyhow!("could not parse server private keys"))?;
+    let key = keys
         .into_iter()
         .next()
-        .unwrap();
+        .ok_or_else(|| anyhow!("expected at least one private key"))?;
 
     let mut config = ClientConfig::new();
     config.ciphersuites = CIPHERSUITES.to_vec();
@@ -40,10 +43,7 @@ fn rustls_config(client: &str) -> Result<ClientConfig, Box<dyn std::error::Error
     Ok(config)
 }
 
-pub async fn make_client(
-    port: u16,
-    client: &str,
-) -> Result<ProcessServiceClient<Channel>, Box<dyn std::error::Error>> {
+pub async fn make_client(port: u16, client: &str) -> Result<ProcessServiceClient<Channel>, Error> {
     let tls = tonic::transport::ClientTlsConfig::new().rustls_client_config(rustls_config(client)?);
 
     let channel = Channel::from_shared(format!("https://localhost:{}", port))?
